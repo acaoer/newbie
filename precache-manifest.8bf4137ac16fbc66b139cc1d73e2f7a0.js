535f09d7250dc402c515f21ed28b04e497e03ff9
acaoer <lycbuct@gmail.com>
2019-01-15 14:09:11 +0800

    Updates

---
+++ precache-manifest.8bf4137ac16fbc66b139cc1d73e2f7a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a8f3c985fbce430faf6",
+    "url": "/newbie/static/js/runtime~main.9a8f3c98.js"
+  },
+  {
+    "revision": "7040172c800a09251008",
+    "url": "/newbie/static/js/main.7040172c.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/newbie/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "7040172c800a09251008",
+    "url": "/newbie/static/css/main.6b287aef.chunk.css"
+  },
+  {
+    "revision": "a7d01ee535b7c98c547b3f4f5048d3be",
+    "url": "/newbie/index.html"
+  }
+];
